e7476dda139534cd4a9423f7b874b2dc38d1e1fb
Zack Balbin <zack@zackbalbin.com>
2022-02-19 01:45:02 -0800

    parse transactionId from box

--- src/main/scala/Box/BoxUtils.scala
+++ src/main/scala/Box/BoxUtils.scala
@@ -3,35 +3,37 @@ package ergonames.Box
 import ergonames.Http.HttpConfig.ergoTestnetAPIUrl
 
 import scalaj.http._
+import org.ergoplatform.appkit.InputBox
 import spray.json._
 import DefaultJsonProtocol._
-import org.ergoplatform.appkit.InputBox
 
 object BoxUtils {
 
     def getBoxFunderAddress(transactionId: String): String = {
         val url: String = ergoTestnetAPIUrl + "/api/v1/transactions/" + transactionId
         val response = Http(url).asString
-        val transactionData = response.body
-        //println(transactionData)
-        "ss"
+        val boxData = response.body
+        val boxAST = boxData.parseJson
+        val boxJson = boxAST.asJsObject
+        val transactionInputs = boxJson.getFields("inputs")
+        val txInputsString = transactionInputs.toString()
+        val txInputsJson = txInputsString.parseJson
+        val txInputsAST = txInputsJson.asJsObject
+        val address = txInputsAST.getFields("address")
+        println(address(0))
+        val funderAddress = "ss"
+        funderAddress
     }
 
     def getBoxTransactionId(box: InputBox): String = {
         val url: String = ergoTestnetAPIUrl + "/api/v1/boxes/" + box.getId()
         val response = Http(url).asString
         val boxData = response.body
-        println(boxData)
-
-        // val jsonData = boxData.parseJson
-        // val mappedData = jsonData.convertTo[Map[String, Option[String]]]
-        // println(mappedData("transactionId"))
-
-        val substr1 = boxData.split("transactionId")(1)
-        val substr2 = substr1.substring(3, substr1.size - 1)
-        val substr3 = substr2.split(",")(0)
-        val substr4 = substr3.substring(0, substr3.size - 1)
-        substr4
+        val boxAST = boxData.parseJson
+        val boxJson = boxAST.asJsObject
+        val transactionIdRaw = boxJson.getFields("transactionId")(0).toString()
+        val transactionId = transactionIdRaw.substring(1, transactionIdRaw.length()-1)
+        transactionId
     }
 
     def printBoxesFromList(boxes: java.util.List[InputBox]) {
@@ -40,7 +42,7 @@ object BoxUtils {
             //println(boxes.get(index))
             println("Box Id:\t\t\t" + boxes.get(index).getId())
             println("Transaction Id:\t\t" + getBoxTransactionId(boxes.get(index)))
-            println("Funder Address:\t\t" + getBoxFunderAddress(getBoxFunderAddress("s")))
+            println("Funder Address:\t\t" + getBoxFunderAddress(getBoxFunderAddress(getBoxTransactionId(boxes.get(index)))))
             println()
         }
     }
