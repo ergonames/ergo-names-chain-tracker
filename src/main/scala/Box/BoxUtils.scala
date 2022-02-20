18c54353c5006fd766b61a88e0c426b89e381b38
Zack Balbin <zack@zackbalbin.com>
2022-02-19 23:05:05 -0800

    track box creation and settlement heights

--- src/main/scala/Box/BoxUtils.scala
+++ src/main/scala/Box/BoxUtils.scala
@@ -40,6 +40,22 @@ object BoxUtils {
         value
     }
 
+    def getCreationHeight(box: InputBox): String = {
+        val url: String = ergoTestnetAPIUrl + "/api/v1/boxes/" + box.getId()
+        val boxData = getData(url)
+        val boxJson = convertStringToJsObject(boxData)
+        val creationHeight = boxJson.getFields("creationHeight")(0).toString()
+        creationHeight
+    }
+
+    def getSettlementHeight(box: InputBox): String = {
+        val url: String = ergoTestnetAPIUrl + "/api/v1/boxes/" + box.getId()
+        val boxData = getData(url)
+        val boxJson = convertStringToJsObject(boxData)
+        val settlementHeight = boxJson.getFields("settlementHeight")(0).toString()
+        settlementHeight
+    }
+
     def printBoxesFromList(boxes: java.util.List[InputBox]) {
         var index: Int = 0
         for ( index <- 0 to boxes.size() - 1) {
@@ -47,11 +63,15 @@ object BoxUtils {
             var transactionId = getBoxTransactionId(boxes.get(index))
             var funderAddress = getBoxFunderAddress(transactionId)
             var boxValue = getBoxValue(boxes.get(index))
+            var creationHeight = getCreationHeight(boxes.get(index))
+            var settlementHeight = getSettlementHeight(boxes.get(index))
             println("Box Number:\t\t" + index)
             println("Box Id:\t\t\t" + boxId)
             println("Transaction Id:\t\t" + transactionId)
             println("Funder Address:\t\t" + funderAddress)
             println("Box Value:\t\t" + boxValue)
+            println("Creation Height:\t" + creationHeight)
+            println("Settlement Height:\t" + settlementHeight)
             println()
         }
     }
